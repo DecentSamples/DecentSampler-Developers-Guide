84c68a6d8e267bfb1a3c0d1092c7f03b989f6612
David Hilowitz <dhilowitz@gmail.com>
2026-08-27 13:32:08 -0400

    Version bump

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ project = 'DecentSampler'
 copyright = '2026, David Hilowitz'
 author = 'David Hilowitz'
 
-release = '1.26.0'
-version = '1.26.0'
+release = '1.27.0'
+version = '1.27.0'
 
 # -- General configuration
 
